f8c253987b2bdb21585646c1a70d53edc62e3a53
김윤수 <yskim@taeha.co.kr>
2020-06-18 08:12:28 +0900

    로그인 기능 추가

--- app/src/main/java/yskim/sample/androidphpmysql/Constants.java
+++ app/src/main/java/yskim/sample/androidphpmysql/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     private static final String ROOT_URL = "http://192.168.30.52/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
 }
