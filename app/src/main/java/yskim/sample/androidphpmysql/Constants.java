7a03d4a94ebef6e9f06b571ffe97836d7bff9806
김윤수 <yskim@taeha.co.kr>
2020-06-17 14:38:50 +0900

    first commit

---
+++ app/src/main/java/yskim/sample/androidphpmysql/Constants.java
@@ -0,0 +1,7 @@
+package yskim.sample.androidphpmysql;
+
+public class Constants {
+    private static final String ROOT_URL = "http://192.168.30.52/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
