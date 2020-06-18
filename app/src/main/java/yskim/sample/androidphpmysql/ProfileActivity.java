d4f1b79303f03d309c13148a9cff3344d1fe4f27
김윤수 <yskim@taeha.co.kr>
2020-06-18 11:42:29 +0900

    로그아웃기능 추가

--- app/src/main/java/yskim/sample/androidphpmysql/ProfileActivity.java
+++ app/src/main/java/yskim/sample/androidphpmysql/ProfileActivity.java
@@ -1,10 +1,14 @@
 package yskim.sample.androidphpmysql;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -25,5 +29,28 @@ public class ProfileActivity extends AppCompatActivity {
 
         textViewUsername.setText(SharedPrefManager.getInstance(this).getUsername());
         textViewUserEmail.setText(SharedPrefManager.getInstance(this).getUserEmail());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.menuSettings:
+                Toast.makeText(this, "You clicked settings", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menuLogout:
+                SharedPrefManager.getInstance(this).logout();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+        return true;
     }
 }
